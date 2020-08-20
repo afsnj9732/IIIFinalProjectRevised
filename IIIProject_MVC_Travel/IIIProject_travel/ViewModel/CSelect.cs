e19121abc9a7399c87bf16d0dabe0b189d7b17f1
User <User@DESKTOP-H3ECMEV>
2020-08-20 18:28:34 +0800

    Travel_style

--- IIIProject_MVC_Travel/IIIProject_travel/ViewModel/CSelect.cs
+++ IIIProject_MVC_Travel/IIIProject_travel/ViewModel/CSelect.cs
@@ -8,7 +8,7 @@ namespace IIIProject_travel.ViewModel
     public class CSelect
     {
         public string order { get; set; }
-        public string border_color { get; set; }
+        public string background_color { get; set; }
         public string contain { get; set; }
     }
 }
