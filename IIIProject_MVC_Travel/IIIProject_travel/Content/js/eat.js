4abafeae5c5491c93a35ed1039e4ee20a9a5b743
deyomoe <icytape620@gmail.com>
2020-08-26 14:38:56 +0800

    fixed rwd

---
+++ IIIProject_MVC_Travel/IIIProject_travel/Content/js/eat.js
@@ -0,0 +1,11 @@
+; (function () {
+    $(".filterbtn").click(function () {
+        $(".btn-outline-warning").toggle('fast');
+    });
+
+    $(".popularMemberbtn").click(function () {
+        $(".popularAvatar").toggle('fast');
+    });
+
+
+})();
