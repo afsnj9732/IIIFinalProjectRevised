bf39dc7aa8ea852da39804de454510270cc6cb62
Yorker <yankees2484@gmail.com>
2020-08-27 13:06:55 +0800

    首頁修改

--- IIIProject_MVC_Travel/IIIProject_travel/Controllers/HomeController.cs
+++ IIIProject_MVC_Travel/IIIProject_travel/Controllers/HomeController.cs
@@ -28,36 +28,36 @@ namespace IIIProject_travel.Controllers
             return View();
         }
 
-        public ActionResult Login()
-        {
-            //string code = Session[CDictionary.SK_USERLOGIN_CODE] as string;
-            //if (string.IsNullOrEmpty(code))
-            //{
-            //    Random r = new Random();
-            //    code = r.Next(0, 10).ToString() + r.Next(0, 10).ToString() + r.Next(0, 10).ToString() + r.Next(0, 10).ToString();
-            //    Session[CDictionary.SK_USERLOGIN_CODE] = code;
-            //}
-            //ViewBag.CODE = code;
-            return View();
-        }
-        [HttpPost]
-        public ActionResult Login(CLogin p)
-        {
+        //public ActionResult Login()
+        //{
+        //    //string code = Session[CDictionary.SK_USERLOGIN_CODE] as string;
+        //    //if (string.IsNullOrEmpty(code))
+        //    //{
+        //    //    Random r = new Random();
+        //    //    code = r.Next(0, 10).ToString() + r.Next(0, 10).ToString() + r.Next(0, 10).ToString() + r.Next(0, 10).ToString();
+        //    //    Session[CDictionary.SK_USERLOGIN_CODE] = code;
+        //    //}
+        //    //ViewBag.CODE = code;
+        //    return View();
+        //}
+        //[HttpPost]
+        //public ActionResult Login(CLogin p)
+        //{
 
-            //string code = Session[CDictionary.SK_USERLOGIN_CODE] as string;
-            //if (!p.txtPassword.Equals(code))
-            //{
-            //    ViewBag.CODE = code;
-            //    return View();
-            //}
-            String fAccount = p.txtAccount;
+        //    //string code = Session[CDictionary.SK_USERLOGIN_CODE] as string;
+        //    //if (!p.txtPassword.Equals(code))
+        //    //{
+        //    //    ViewBag.CODE = code;
+        //    //    return View();
+        //    //}
+        //    String fAccount = p.txtAccount;
             
-            tMember cust = (new dbJoutaEntities()).tMember.FirstOrDefault(t => t.f會員帳號== fAccount && t.f會員密碼.Equals(p.txtPassword));
-            if (cust == null)
-                return View();
-            Session[CDictionary.SK_LOGIN_MEMBER] = cust;
-            return RedirectToAction("Home");
-        }
+        //    tMember cust = (new dbJoutaEntities()).tMember.FirstOrDefault(t => t.f會員帳號== fAccount && t.f會員密碼.Equals(p.txtPassword));
+        //    if (cust == null)
+        //        return View();
+        //    Session[CDictionary.SK_LOGIN_MEMBER] = cust;
+        //    return RedirectToAction("Home");
+        //}
         [HttpGet]
         public ActionResult Register()
         {
@@ -172,29 +172,14 @@ namespace IIIProject_travel.Controllers
             //smtp.Send(message);
         }
 
-        //public ActionResult Save()
-        //{
-        //    tMember x = new tMember();
-        //    x.f會員帳號 = Request.Form["txtAccount"];
-        //    x.f會員密碼 = Request.Form["txtPassword"];
-        //    x.f性別 = Request.Form["txtGender"];
-        //    x.f暱稱 = Request.Form["txtNickname"];
-        //    x.f電子郵件 = Request.Form["txtEmail"];
-
-        //    dbJoutaEntities db = new dbJoutaEntities();
-        //    db.tMember.Add(x);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Login");
-        //}
-
         public ActionResult About()
         {
             return View();
         }
 
-        public ActionResult Contact()
-        {
-            return View();
-        }
+        //public ActionResult Contact()
+        //{
+        //    return View();
+        //}
     }
 }
