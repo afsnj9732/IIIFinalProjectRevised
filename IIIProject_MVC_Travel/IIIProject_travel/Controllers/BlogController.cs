7b81b2579a3142f854c0a20ad7d052ca2ca944ff
User <User@DESKTOP-H3ECMEV>
2020-09-01 11:36:54 +0800

    Blog Read more

--- IIIProject_MVC_Travel/IIIProject_travel/Controllers/BlogController.cs
+++ IIIProject_MVC_Travel/IIIProject_travel/Controllers/BlogController.cs
@@ -30,11 +30,11 @@ namespace IIIProject_travel.Controllers
 
 
         }
-        public ActionResult BlogContent()
+        public ActionResult BlogContent(int id)
         {
 
             var article = from t in (new dbJoutaEntities()).tActivity
-                          where t.f活動類型 == "文章"
+                          where t.f活動類型 == "文章"&&t.f活動編號==id
                           select t;
 
 
