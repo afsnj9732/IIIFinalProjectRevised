95a5fa6dacab750d05268064fc2a4dc9209290c9
deyomoe <icytape620@gmail.com>
2020-09-01 20:37:36 +0800

    ...no

--- IIIProject_MVC_Travel/IIIProject_travel/Scripts/IIIProject/eat.js
+++ IIIProject_MVC_Travel/IIIProject_travel/Scripts/IIIProject/eat.js
@@ -1,4 +1,4 @@
-; (function () {
+(function () {
     $(".filterbtn").click(function () {
         $(".btn-outline-warning").toggle('fast');
     });
@@ -7,5 +7,24 @@
         $(".popularAvatar").toggle('fast');
     });
 
+    function getAJAX() {
+        p = JSON.stringify({
+            "order": order, "background_color": background_color, "contain": contain
+            , "category": category, "label": label
+        });
+
+        $.ajax({
+            async: false,
+            url: "/Eat/eatArticleAjax",
+            type: "POST",
+            data: {
+                "p": p
+            },
+            success: function (data) {
+                $("#eatArticleAjax div").remove();
+                $("#eatArticleAjax").append(data);
+            }
+        });
+    };
 
 })();
