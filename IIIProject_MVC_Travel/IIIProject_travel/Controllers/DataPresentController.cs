35856dd4ec3a3992f108edba3c4ee30d219176d7
HuHubert <usogood717@gmail.com>
2020-09-02 19:59:57 +0800

    試整合圖表顯示

---
+++ IIIProject_MVC_Travel/IIIProject_travel/Controllers/DataPresentController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace IIIProject_travel.Controllers
+{
+    public class DataPresentController : Controller
+    {
+        // GET: DataPresent
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetData()
+        {
+            dbJoutaEntities context = new dbJoutaEntities();
+
+            //var query = context.tActivity.Include("f活動預算")
+            //.GroupBy(p => p.Product.ProductName)
+            //.Select(g => new { name = g.Key, count = g.Sum(w => w.Quantity) }).ToList();
+            //return Json(query, JsonRequestBehavior.AllowGet);
+            return View();
+
+        }
+
+
+    }
+}
