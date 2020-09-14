3f44443a170ffe6b56806665c263dbb13bceeec7
ts00800966 <ts00800966@yahoo.com.tw>
2020-09-14 11:57:45 +0800

    mail回覆

--- IIIProject_MVC_Travel/IIIProject_travel/tComment.cs
+++ IIIProject_MVC_Travel/IIIProject_travel/tComment.cs
@@ -17,7 +17,7 @@ namespace IIIProject_travel
         public int fID { get; set; }
         public string f電子郵件 { get; set; }
         public string f電話 { get; set; }
-        public string f名稱 { get; set; }
+        public string f標題 { get; set; }
         public string f意見 { get; set; }
         public string f性別 { get; set; }
         public string f聯絡人 { get; set; }
