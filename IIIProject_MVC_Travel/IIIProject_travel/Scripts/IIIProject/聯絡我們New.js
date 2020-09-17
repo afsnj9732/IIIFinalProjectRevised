75932c4aec3731f4d7dad622f439a726449468ea
ray851015 <62749505+ray851015@users.noreply.github.com>
2020-09-17 20:29:44 +0800

    聯絡我們demo鍵
    圖上

--- IIIProject_MVC_Travel/IIIProject_travel/Scripts/IIIProject/聯絡我們New.js
+++ IIIProject_MVC_Travel/IIIProject_travel/Scripts/IIIProject/聯絡我們New.js
@@ -5,3 +5,10 @@
         document.idform.txtMail.value = "";
     }
 }
+$("#fordemo").click(function () {
+    $("#title").val("行程太過於攏長");
+    $("#post聯絡人").val("十元瑞");
+    $("#phone").val("0976703669");
+    $("#comment").val("今天我們去六福村的時候，前前後後帶了我們進去禮品店購物了3次，真的是夠了!!");
+   
+});
